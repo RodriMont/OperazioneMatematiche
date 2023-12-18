498b362f9c0b15ef8f1e4a7ad7da88cf6c3db339
Rodrigo Sebastian Montero Mozombite <rodrigo.montero@itsrizzoli.it>
2023-12-18 16:20:16 +0100

    sostrazione

--- src/OperazioneMatematiche.java
+++ src/OperazioneMatematiche.java
@@ -8,18 +8,34 @@ public class OperazioneMatematiche {
 
         System.out.println("Inserisce 2 numeri: ");
 
+        System.out.println("Numero a: ");
         int a = scanner.nextInt();
+        System.out.println("Operazione a: ");
+        String operazione = scanner.nextLine();
+        System.out.println("Numero b: ");
         int b = scanner.nextInt();
 
-        int risultato = somma(a, b);
 
-        System.out.println( a + " + " + b + " = " + risultato);
+        if(operazione.equals("+")){
+            int risultato = somma(a, b);
+            System.out.println( a + " + " + b + " = " + risultato);
+
+        }else if(operazione.equals("-")){
+            int risultato = sosttrazione(a, b);
+            System.out.println( a + " - " + b + " = " + risultato);
+        }
+
+        
         scanner.close();
     }
 
     public static int somma(int a, int b){
 
         return a + b;
+    }
+    public static int sosttrazione(int a, int b){
+
+        return a - b;
     }
     
 }
