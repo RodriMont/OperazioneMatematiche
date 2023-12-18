c7db5d9679a49e1616c7e740e842231ec67240c5
Rodrigo Sebastian Montero Mozombite <rodrigo.montero@itsrizzoli.it>
2023-12-18 16:01:37 +0100

    creazione metodo somma

--- src/OperazioneMatematiche.java
+++ src/OperazioneMatematiche.java
@@ -1,5 +1,23 @@
-public class App {
+import java.util.Scanner;
+
+public class OperazioneMatematiche {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserisce 2 numeri: ");
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        int risultato = somma(a, b);
+
+        System.out.println("La somma è " + risultato);
+        scanner.close();
     }
+
+    public static int somma(int a, int b){
+
+        return a + b;
+    }
+    
 }
